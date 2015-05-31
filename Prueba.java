11b102313f789b7e9aa638d44ea30ba234987d4b
Guillermo Mora <guillermijas@gmail.com>
2015-05-31 21:08:53 +0200

    Ejercicio 5

--- Prueba.java
+++ Prueba.java
@@ -4,11 +4,11 @@ import org.junit.*;
 
 
 public class Prueba {
-	
+
 	private BinaryString bitset1, bitset2, bitset3, bitset4, bitset5; 
 	private String pruebaString2 = "1010101010", pruebaString3 = "100101", 
 			pruebaString4 = "0110101", pruebaString5 = "1111000";	
-	
+
 	@Before
 	public void antesDelTest(){
 		bitset1 = new BinaryString(10);
@@ -17,29 +17,47 @@ public class Prueba {
 		bitset4 = new BinaryString(pruebaString4);
 		bitset5 = new BinaryString(pruebaString5);
 	}
-	
+
 
 	@Test
 	public void Test(){
 		assertEquals(10, bitset1.length());
 		assertEquals(10, bitset2.length());
 	}
-	
+
 	@Test
 	public void testSetBit(){
 		bitset3.set(1, '1');
 		assertEquals('1', bitset3.get(1));
 	}
-	
+
 	@Test(expected = BinaryStringException.class)
 	public void testGetOutOfLimitBit(){
 		bitset4.get(56);
 	}
-	
+
 	@Test(expected = BinaryStringException.class)
 	public void testSetOutOfLimitBit(){
 		bitset4.set(56, '0');
 	}
-	
-	
+
+	@Test
+	public void testSetOutOfLimitV2(){
+		try{
+			bitset4.set(56, '0');
+			fail("Se esperaba BinaryStringException");
+		}catch(BinaryStringException e){}
+	}
+
+
 }
+
+
+
+
+
+
+
+
+
+
